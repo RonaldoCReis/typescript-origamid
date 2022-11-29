73516646c9365e71f8f0f6e9b3fc369908857d5e
Ronaldo Reis <ronaldocreis7@gmail.com>
2022-11-29 18:48:25 -0300

    instance of HTMLAnchor

--- scripts.ts
+++ scripts.ts
@@ -1,32 +1,6 @@
-interface cursoType {
-  aulas: number;
-  gratuito: boolean;
-  horas: number;
-  idAulas: number[];
-  nivel: 'iniciante' | 'avançado';
-  nome: string;
-  tags: string[];
-}
+const link = document.getElementById('origamid');
 
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const cursos: cursoType[] = await response.json();
-  mostrarCursos(cursos);
-}
-fetchCursos();
-
-function mostrarCursos(cursos: cursoType[]) {
-  cursos.forEach((curso) => {
-    document.body.innerHTML += `
-      <h1 style="color: ${curso.nivel === 'iniciante' ? 'red' : 'blue'}">${
-      curso.nome
-    }</h1>
-      <p>${curso.nivel}</p>
-      <p>Aulas: ${curso.aulas}</p>
-      <p>Horas: ${curso.horas}h</p>
-      <p>Gratuito: ${curso.gratuito ? 'Sim' : 'Não'}</p>
-      <p>Tags: ${curso.tags.join(', ')}</p>
-      <p>Id Aulas: ${curso.idAulas.join(' | ')}</p>
-    `;
-  });
+if (link instanceof HTMLAnchorElement) {
+  link.href = link.href.replace('http://', 'https://');
+  console.log(link.href);
 }
