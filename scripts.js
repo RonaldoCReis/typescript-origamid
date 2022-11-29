8acf0dbd52a99d645dfc9549bb87e8d5b1d49162
Ronaldo Reis <ronaldocreis7@gmail.com>
2022-11-29 20:06:17 -0300

    Eventos e callback

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 "use strict";
-const link = document.getElementById('origamid');
-if (link instanceof HTMLAnchorElement) {
-    link.href = link.href.replace('http://', 'https://');
-    console.log(link.href);
-}
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const button = document.querySelector('button');
+const toggleNav = () => {
+    const nav = document.querySelector('nav');
+    if (nav && button) {
+        nav?.classList.toggle('active');
+        const ariaLabel = nav?.classList.contains('active')
+            ? 'Fechar Menu'
+            : 'Abrir Menu';
+        button?.setAttribute('aria-expanded', nav?.classList.contains('active').toString());
+        button?.setAttribute('aria-label', ariaLabel);
+    }
+};
+button?.addEventListener('click', toggleNav);
